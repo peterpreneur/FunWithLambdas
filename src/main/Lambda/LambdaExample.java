e52fab0390646beb650def8da7e744ac29df8efd
pas8 <peterpreneur@gmail.com>
2022-01-27 13:04:13 -0600

    test

--- src/main/Lambda/LambdaExample.java
+++ src/main/Lambda/LambdaExample.java
@@ -1,9 +1,46 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.Test;
+
 public class LambdaExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+	Long id = 0L;
+	
+	@Test
+	public void test()
+	{
+		List<Transaction> transactions = new ArrayList<>();
+		
+		for (int i=0; i<200; i++) {
+			transactions.add(buildTransaction());
+		}
+			
+		Collections.sort(transactions);
+	}
+
+	private Transaction buildTransaction() {
+		id ++;
+		
+		Transaction transaction = new Transaction();
+		transaction.setId(id);
+		transaction.setAmount(generateRandomAmount());
+		
+		long millis = ThreadLocalRandom.current().nextLong(1400000000000L, 1500000000000L);
+		transaction.setDate(new Date(millis));
+		
+		return transaction;
+	}
+	
+	private double generateRandomAmount() {
+		double amount = ThreadLocalRandom.current().nextDouble() * 1000;
+		amount = amount * 100.0;
+		amount = Math.round(amount);
+		return amount;
 	}
 }
